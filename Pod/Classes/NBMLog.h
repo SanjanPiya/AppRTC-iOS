d1a0a7170f5d2a95f0f28adf9e65a1050c7dd775
Nico Krause <inspiraluna@gmail.com>
2017-03-06 23:07:33 +0530

    was a problem during integration

--- Pod/Classes/NBMLog.h
+++ Pod/Classes/NBMLog.h
@@ -14,8 +14,7 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-
-#import "CocoaLumberjack.h"
+#import <CocoaLumberjack/CocoaLumberjack.h>
 
 #ifndef NBMLogsEnabled
     #ifdef DEBUG
